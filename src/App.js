e390a939b9ab15b4cb2a723dfcff0f7284ad1650
unknown <amy_chan@student.uml.edu>
2021-04-05 13:06:31 -0400

    sidebar menu added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/Sidebar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <div className="sidebar">
+                <Sidebar />
+            </div>
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,85 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Image from "react-bootstrap/Image";
+import projectIcon from "../img/projectIcon.png";
+import playerIcon from "../img/navIcons/playerIcon.png";
+import skillsIcon from "../img/navIcons/skillsIcon.png";
+import friendshipIcon from "../img/navIcons/friendshipIcon.png";
+import craftingIcon from "../img/navIcons/craftingIcon.png";
+import itemShippedIcon from "../img/navIcons/itemShippedIcon.png";
+import fishIcon from "../img/navIcons/fishIcon.png";
+import artifactsIcon from "../img/navIcons/artifactsIcon.png";
+import mineralsIcon from "../img/navIcons/mineralsIcon.png";
+import cookingIcon from "../img/navIcons/cookingIcon.png";
+import ccBundleIcon from "../img/navIcons/ccBundleIcon.png";
+import eradicationIcon from "../img/navIcons/eradicationIcon.png";
+import museumIcon from "../img/navIcons/museumIcon.png";
+import grandpaIcon from "../img/navIcons/grandpaIcon.png";
+
+const Sidebar = () => {
+    return (
+        <Nav defaultActiveKey="/home" className="flex-column" variant="pills">
+            {/* Nav Header */}
+            <Nav.Item className="navHeader">
+                <Image className="navHeaderIcon" src={projectIcon} />
+                <span className="navHeaderText">sdv-checker</span>
+            </Nav.Item>
+
+            {/* Nav Menu */}
+            <Nav.Link href="/home">
+                <Image className="navIcon" src={playerIcon} />
+                <span className="navText">Farmer</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-1">
+                <Image className="navIcon" src={skillsIcon} />
+                <span className="navText">Skills</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-2">
+                <Image className="navIcon" src={friendshipIcon} />
+                <span className="navText">Friendship</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-3">
+                <Image className="navIcon" src={craftingIcon} />
+                <span className="navText">Crafting</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-4">
+                <Image className="navIcon" src={itemShippedIcon} />
+                <span className="navText">Items Shipped</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-5">
+                <Image className="navIcon" src={fishIcon} />
+                <span className="navText">Fish</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-6">
+                <Image className="navIcon" src={artifactsIcon} />
+                <span className="navText">Artifacts</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-7">
+                <Image className="navIcon" src={mineralsIcon} />
+                <span className="navText">Minerals</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-8">
+                <Image className="navIcon" src={cookingIcon} />
+                <span className="navText">Cooking</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-9">
+                <Image className="navIcon" src={ccBundleIcon} />
+                <span className="navText">Community Center</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-10">
+                <Image className="navIcon" src={eradicationIcon} />
+                <span className="navText">Eradication Goals</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-11">
+                <Image className="navIcon" src={museumIcon} />
+                <span className="navText">Museum</span>
+            </Nav.Link>
+            <Nav.Link eventKey="link-12">
+                <Image className="navIcon" src={grandpaIcon} />
+                <span className="navText">Grandpa's Evaluation</span>
+            </Nav.Link>
+        </Nav>
+    );
+};
+
+export default Sidebar;
