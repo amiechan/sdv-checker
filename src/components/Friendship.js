6232c571a4650b03ac4ee576b2bd85e267ef037c
Xuhao Wang <wxuhao@gmail.com>
2021-04-09 21:01:33 -0400

    prase the lord??????

--- src/components/Friendship.js
+++ src/components/Friendship.js
@@ -17,46 +17,33 @@ const Friendship = ({ data }) => {
             <>
               {friendList.map((friend) => (
                 <>
-                  <Col className="col-xl-3 col-6 p-1">
-                    <Card>
-                      <Card.Body className="p-2">
+                    {friendList.map((friend) => (
                         <>
-                          <div className="d-flex justify-content-between px-md-1">
-                            <h4 className="text-primary mb-1"><a href={"https://stardewvalleywiki.com/" + friend[0]} target="_blank">{friend[0]}</a></h4>
-                            <p className="mb-0">Level: { }/10</p>
-                            <img src={`${process.env.PUBLIC_URL}/img/hearts/` + Math.floor(friend[1] / 250) + `_hearts.png`} className="img-fluid" alt="Hearts" style={{ maxHeight: '9px', maxWidth: '119px' }} />
-                            <div className="align-self-center">
-                              <img src={`${process.env.PUBLIC_URL}/img/villagers/` + friend[0] + `.png`} className="img-fluid" alt={friend[0]} style={{ maxHeight: '75px', maxWidth: '75px' }} />
-                            </div>
-                          </div>
-                          <div className="px-md-1">
-                            <div className="progress mt-1 mb-1 rounded" style={{ height: '7px' }}>
-                              <div className="progress-bar bg-danger" role="progressbar" style={{ width: '${friendPercent}%' }} aria-valuenow="${friendPercent}" aria-valuemin={0} aria-valuemax={100} />
-                            </div>
-                            <p className="mb-0">Exp: {friend[1]}/250</p>
-                          </div>
-                        </>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                  <Accordion defaultActiveKey="0">
-                    <Card>
-                      <Accordion.Toggle as={Card.Header} eventKey="0">
-                        Click me!
-                    </Accordion.Toggle>
-                      <Accordion.Collapse eventKey="0">
-                        <Card.Body>Hello! I'm the body</Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                    <Card>
-                      <Accordion.Toggle as={Card.Header} eventKey="1">
-                        Click me!
-                    </Accordion.Toggle>
-                      <Accordion.Collapse eventKey="1">
-                        <Card.Body>Hello! I'm another body</Card.Body>
-                      </Accordion.Collapse>
-                    </Card>
-                  </Accordion>
+                        <Col className="col-xl-3 col-6 p-1">
+                          <Card>
+                            <Card.Body className="p-2">
+                              <>
+                              <div className="d-flex justify-content-between px-md-1">
+                                  <h4 className="text-primary mb-1"><a href={"https://stardewvalleywiki.com/" + friend['string']} target="_blank">{friend['string']}</a></h4>
+                                  <p className="mb-0">Level: {}/10</p>
+                                  <img src={`${process.env.PUBLIC_URL}/img/hearts/` + Math.floor(friend['Points']/250) + `_hearts.png`} className="img-fluid" alt="Hearts" style={{ maxHeight: '9px', maxWidth: '119px' }} />
+                                <div className="align-self-center">
+                                  <img src={`${process.env.PUBLIC_URL}/img/villagers/` + friend['Points'] + `.png`} className="img-fluid" alt={friend['Points']} style={{ maxHeight: '75px', maxWidth: '75px' }} />
+                                </div>
+                              </div>
+                              <div className="px-md-1">
+                                <div className="progress mt-1 mb-1 rounded" style={{ height: '7px' }}>
+                                  <div className="progress-bar bg-danger" role="progressbar" style={{ width: '${friendPercent}%' }} aria-valuenow="${friendPercent}" aria-valuemin={0} aria-valuemax={100} />
+                                </div>
+                                <p className="mb-0">Exp: {friend['Points']}/250</p>
+                              </div>
+                              </>
+                            </Card.Body>
+                          </Card>
+                        </Col>
+                
+                      </>
+                    ))}
                 </>
               ))}
             </>
---
+++ src/components/parse.js
@@ -0,0 +1,19 @@
+function parse(data, tags) {
+    let root = data.documentElement;
+    let children = root.childNodes;
+    let itemArray = [];
+
+    children.forEach( (child, childIndex) => {
+        // Make a new dict for each child
+        let  attrDict = {};
+        tags.forEach(tag => {
+            // Insert a tag:value pair for each tag
+            attrDict[tag] = data.getElementsByTagName(tag)[childIndex].textContent;
+        })
+        // Add the dict to the main array
+        itemArray.push(attrDict);
+    })
+    return itemArray;
+}
+
+export default parse;
