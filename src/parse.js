fdbc3fd4e27f41e95dd4bd1794b1b750988c17cd
Xuhao Wang <wxuhao@gmail.com>
2021-04-09 21:13:50 -0400

    oops wrong parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,15 +1,18 @@
 function parse(data, tags) {
-    var root = data.documentElement;
-    var children = root.childNodes;
-    var itemArray = [];
-    console.log(tags);
+    let root = data.documentElement;
+    let children = root.childNodes;
+    let itemArray = [];
+
     children.forEach( (child, childIndex) => {
-        let attrDict = {};
+        // Make a new dict for each child
+        let  attrDict = {};
         tags.forEach(tag => {
+            // Insert a tag:value pair for each tag
             attrDict[tag] = data.getElementsByTagName(tag)[childIndex].textContent;
         })
+        // Add the dict to the main array
         itemArray.push(attrDict);
-    });
+    })
     return itemArray;
 }
 
