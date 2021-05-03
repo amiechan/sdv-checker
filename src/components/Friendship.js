31734d4664bece7185c7dc11fd70184b82a84653
Xuhao Wang <wxuhao@gmail.com>
2021-05-03 00:11:57 -0400

    item icons and favorites list

--- src/components/Friendship.js
+++ src/components/Friendship.js
@@ -3,6 +3,7 @@ import {
   Image, Row, Col, Card, ProgressBar, Accordion
 } from "react-bootstrap";
 import parse from "../parse";
+//import returnFavories from "../data/friend_favorites.js";
 
 const Friendship = ({ data }) => {
   if (data !== "") {
@@ -13,25 +14,28 @@ const Friendship = ({ data }) => {
     let friendList = parse(friendshipData, tags);
     // Henchman isn't a friend
     friendList = friendList.filter(friend => friend["string"] !== 'Henchman');
-
+    //let friends = returnFavories();
+    //friends['Alex'].map(arr => {
+    //  console.log(arr);
+    //});
     return (
       <Card body className="contentCard fileDiv">
         <h5>Friendship</h5>
         <Row>
           <>
             {friendList.map((friend, index) => (
-              <Col className="p-1">
+              <Col key={index} className="p-1">
                 <Accordion>
                   <Card className="friendCard">
                     <Accordion.Toggle className="p-0" as={Card.Header} eventKey="0">
                       <Card body key={index} className="friendCard">
                         <Card.Title><a href={"https://stardewvalleywiki.com/" + friend['string']} target="_blank">{friend['string']}</a></Card.Title>
-                        <Card.Img fluid variant="top" src={`${process.env.PUBLIC_URL}/img/villagers/` + friend['string'] + `.png`} />
+                        <Card.Img fluid="true" variant="top" src={`${process.env.PUBLIC_URL}/img/villagers/` + friend['string'] + `.png`} />
                         <Card.Text className="text-center">
                           <br />
                           {friend['Points'] % 250} / 250
                           <ProgressBar className="mb-3" max="250" variant="danger" now={(friend['Points'] % 250)} />
-                          <Image fluid className="heartResize" src={`${process.env.PUBLIC_URL}/img/hearts/` + Math.floor(friend['Points'] / 250) + `_hearts_stack.png`}></Image>
+                          <Image fluid="true" className="heartResize" src={`${process.env.PUBLIC_URL}/img/hearts/` + Math.floor(friend['Points'] / 250) + `_hearts_stack.png`}></Image>
                         </Card.Text>
                       </Card>
                     </Accordion.Toggle>
